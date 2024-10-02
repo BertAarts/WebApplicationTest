435fa7c336f78158390845924a594f967cac3998
Bert Aarts <b.aarts@fontys.nl>
2024-10-02 09:47:02 +0200

    Add project files.

---
+++ WebApplicationTest/Models/Student.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApplicationTest.Models
+{
+    public class Student
+    {
+        public Student(string name, string place)
+        {
+            Name = name;
+            Place = place;
+        }
+    
+        public string Name { get;set; }
+        public string Place {  get;set; }
+
+    }
+}
